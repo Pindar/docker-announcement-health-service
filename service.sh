7de2ddc393a917e3a3f69b45e6557a6a5d4efc34
Simon Dittlmann <simon@itnotes.de>
2015-02-18 23:51:37 +0100

    fix announcement script

--- service.sh
+++ service.sh
@@ -1,12 +1,11 @@
-
 #!/bin/bash
 
 export ETCD_PORT=${ETCD_PORT:-4001}
 export HOST_IP=${HOST_IP:-172.17.42.1}
 export ETCD=$HOST_IP:$ETCD_PORT
 
-if [[ $ENVIRONMENT == "" || $NUMBER == "" || $SERVICE == "" || $ANNOUNCE_VALUE == "" || $HEALTH_URL ]]; then
-  echo "[announce-health] please specify ENVIRONMENT, NUMBER and SERVICE";
+if [[ $ENVIRONMENT == "" || $NUMBER == "" || $SERVICE == "" || $ANNOUNCE_VALUE == "" || $HEALTH_URL == "" ]]; then
+  echo "[announce-health] please specify ENVIRONMENT, NUMBER, SERVICE, ANNOUNCE_VALUE, HEALTH_URL";
   exit 1;
 fi
 
@@ -17,26 +16,30 @@ TTL=$((THRESHOLD*TIMEOUT))
 echo "[announce-health] Start announce service for $SERVICE";
 
 i=0;
-while true && i++; do
+while true; do
 
   if [[ $((i % 3)) -eq 0 ]]; then
     failure=0    
   fi;
 
+  echo "[announce-health] check";
+
   if [[ `curl --silent --max-time 60 $HEALTH_URL -D - -O | grep "200 OK"` ]]; then
-      etcdctl set --peers $ETCD /announce/services/$SERVICE/$ENVIRONMENT/$NUMBER $ANNOUNCE_VALUE --ttl $TTL;
-      etcdctl set --peers $ETCD /health/services/$SERVICE/$ENVIRONMENT/$NUMBER OK --ttl $TTL;
+      echo "[announce-health] Service $SERVICE success";
+      etcdctl --peers $ETCD set /announce/services/$SERVICE/$ENVIRONMENT/$NUMBER "$ANNOUNCE_VALUE" --ttl $TTL;
+      etcdctl --peers $ETCD set /health/services/$SERVICE/$ENVIRONMENT/$NUMBER OK --ttl $TTL;
     else
       failure+=1
       if [ $failure > $THRESHOLD ]; then
         echo "[announce-health] Service $SERVICE failed $failure times in a row";
-        etcdctl rm --peers $ETCD /announce/services/$SERVICE/$ENVIRONMENT/$NUMBER ;
-        etcdctl set --peers $ETCD /health/services/$SERVICE/$ENVIRONMENT/$NUMBER FAILURE --ttl $TTL;
+        etcdctl --peers $ETCD rm /announce/services/$SERVICE/$ENVIRONMENT/$NUMBER ;
+        etcdctl --peers $ETCD set /health/services/$SERVICE/$ENVIRONMENT/$NUMBER FAILURE --ttl $TTL;
       fi;
     fi;
 
+  i=$((i+1));
 	sleep $TIMEOUT;
 done
 
-etcdctl rm --peers $ETCD /announce/services/ingress/$ENVIRONMENT/$NUMBER --recursive
-etcdctl rm --peers $ETCD /health/services/$SERVICE/$ENVIRONMENT/$NUMBER
+etcdctl --peers $ETCD rm /announce/services/ingress/$ENVIRONMENT/$NUMBER --recursive
+etcdctl --peers $ETCD rm /health/services/$SERVICE/$ENVIRONMENT/$NUMBER
